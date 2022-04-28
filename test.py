dfdf719aee4e6f56381dd32a71cc25474fda9559
Fujii Noritsugu <quercus491@gmail.com>
2022-04-29 04:38:55 +0900

    ブランチの確認

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+def test_branch():
+    pass
+
 def main():
     print('Hello Python')
     for i in range(10):
         print(i)
     print('Hello Pull Request')
+    test_branch()
 
 if __name__ == '__main__':
     main()
